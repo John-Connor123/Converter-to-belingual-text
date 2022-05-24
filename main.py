d01f454a58c19655666ab73ad01eb558ab124d59
Евдокимов Иван Владимирович <ivevdokimov@miem.hse.ru>
2022-05-25 02:57:06 +0300

    Updated start text

--- main.py
+++ main.py
@@ -294,23 +294,28 @@ def get_transcription():
 
 
 if __name__ == '__main__':
-    print("""              Description:
+    print("""                                        Description:
      This program make a Bilingual text from English text and it doesn't need a transcription.
-    Just put your file to a directory where program is. My program do all other work.
+    Just put your file to a directory where program is and answer in questions in console.
+    My program do all other work.
     
      Program works with txt, epub, fb2 formats. 
     But if your file has a fb2 format you should choose your file in the folder window which will open later.
     
      Output file has a name 'Bilingual_(input file name).docx'. 
-    Other files are temp. You can delete its if you don't need its.
+    File with only transcribed text and replace settings are temp. You can delete package Temp_file if you don't need it.
     
-                    Work_details:
-     What does program do? Find your file in directory where is this program and
-    use "chromedriver" to open Google for opening web-site "https://tophonetics.com" and translate your file there.
+                                              Work_details:
+     What does program do? Find your file(for example: it can be a Spanish book) in directory where is this program,
+     get translate on another language(for example: on Japanese), and create sentence by sentence bilingual text.
+    You can add transcribed english text. The program will use "chromedriver" to open Google for opening web-site "https://tophonetics.com"
+    and translate your file there(if you choose this option).
     (Works only with Google browser)
-    Translate speed is about 250 symbols at 1 second.
+    You can change manage replaces at file 'Transcription_replaces.txt'. 
+    If you want to delete all replaces then the file should be with:{}
+    Transcription speed is about 250 symbols per second.
     P.s. If web-site doesn't loading please run program later 
-    because there are rare situations when web-site doesn't available. 
+    because there are rare situations when web-site doesn't available.
      Contacts: ivan.eudokimoff2014@gmail.com
     
     Thanks for using my program:)\n\n""")
